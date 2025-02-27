4d760e0079667ae4da7db3414b5b34449ae91d4a
Sazid Fahim <sazidfahim49@gmail.com>
2025-02-28 00:20:38 +0600

    1.SMS Issue Solve 2.Card Migration 3.Single Card store & show

---
+++ app/Models/Card.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    protected $table = 'cards';
+
+    protected $fillable = [
+        'creator_id',
+        'card_number',
+        'price',
+        'start_date',
+        'end_date',
+        'status',
+        'assign_id'
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
+    public function wardAdmin()
+    {
+        return $this->belongsTo(User::class, 'assign_id');
+    }
+}
